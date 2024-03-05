d39dee076a1481c1d1ca91f8fb36c16a316042c5
alohasnail <harrysnail@hotmail.com>
2024-03-05 20:32:44 +0800

    update dependencies in pom.xml and add the initial ProductDTO class

---
+++ src/main/java/com/citi/product/dto/ProductDTO.java
@@ -0,0 +1,15 @@
+package com.citi.product.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDTO {
+    private Long productId;
+    private String name;
+    private Double price;
+    private String category;
+}
